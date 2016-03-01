025b7811aa90443b622acc5882b7c0541383aa4d
Blaise <blaise@untzag.com>
2016-03-01 15:53:56 -0600

    fix opa800 process_motortune
    version 1.6.5
    - fixed opa800 process motortune
    - added aspect to artists.mpl_2D
    - sha no-longer stored in main.ini

--- __init__.py
+++ __init__.py
@@ -14,27 +14,23 @@ import ConfigParser as _ConfigParser
 # get config
 _ini_path = _os.path.join(_wt_dir, 'main.ini')
 _config = _ConfigParser.SafeConfigParser()
-_config.read(_ini_path) 
+_config.read(_ini_path)
 
-# attempt to update ini
+# attempt get git sha
 try:
     _HEAD_file = _os.path.join(_wt_dir, '.git', 'logs', 'HEAD')
     with open(_HEAD_file) as _f:
         for _line in _f.readlines():
             _sha = _line.split(' ')[1]  # most recent commit is last
-    _sha.encode('ascii','ignore')
-    _config.set('main', 'git sha', _sha)
-    with open(_ini_path, 'w') as _ini:    
-        _config.write(_ini)
 except:
-    pass
+    _sha = '0000000'
 
 # WrightTools version format: a.b.c.d
 # a - major release
 # b - minor release
 # c - bugfix
 # d - git sha key
-__version__ = _config.get('main', 'version') + '.' + _config.get('main', 'git sha')[:7]
+__version__ = _config.get('main', 'version') + '.' + _sha[:7]
 
 ## iterate ####################################################################
 
