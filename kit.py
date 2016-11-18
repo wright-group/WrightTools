5dc308609e58ecde56fe661566be392f1e916854
Blaise Thompson <blaise@untzag.com>
2016-11-18 13:18:34 -0600

    fix string2array
    version 2.3.3
    - string2array should work again

--- kit.py
+++ kit.py
@@ -1099,12 +1099,12 @@ def string2array(string, sep='\t'):
     for i in range(1, dimensionality+1)[::-1]:
         to_match = '['*(i-1)
         count_positive = string.count(to_match + ' ')
-        cout_negative = string.count(to_match + '-')
-        shape.append(count_positive + cout_negative)
+        count_negative = string.count(to_match + '-')
+        shape.append(count_positive + count_negative)
     shape[-1] = size / shape[-2]
     for i in range(1, dimensionality-1)[::-1]:
         shape[i] = shape[i] / shape[i-1]
-    shape = tuple(shape)
+    shape = tuple([int(s) for s in shape])
     # import list of floats
     l = string.split(' ')
     l = flatten_list([i.split('-') for i in l])  # annoyingly series of negative values get past previous filters
@@ -1132,7 +1132,7 @@ def string2item(string, sep='\t'):
     else:
         split = string.split(sep)
         if split[0][0:2] == '[[':  # case of multidimensional arrays
-            out = string2array(split[1][:])
+            out = string2array(sep.join(split))
         else:
             split = [i.strip() for i in split]  # remove dumb things
             split = [i if i is not '' else 'None' for i in split]  # handle empties
