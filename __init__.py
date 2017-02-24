af0fe0c575c1e1a7e1f1303d230b430dab0cfff4
Blaise Thompson <blaise@untzag.com>
2017-02-23 19:12:10 -0600

    fix import order error
    version 2.8.1
    - fix import oder error (only a problem on new installs)

--- __init__.py
+++ __init__.py
@@ -1,10 +1,21 @@
-### import ####################################################################
-
-
-from __future__ import absolute_import, division, print_function, unicode_literals
+### create temp folder if none exists #########################################
 
 import os as _os
-import sys as _sys
+_temp_dir = _os.path.join(_os.path.dirname(__file__), 'temp')
+if not _os.path.isdir(_temp_dir):
+    _os.mkdir(_temp_dir)
+
+
+### version ###################################################################
+
+
+# MAJOR.MINOR.PATCH (semantic versioning)
+# major version changes may break backwords compatibility
+__version__ = '2.8.1'  
+
+
+### import ####################################################################
+
 
 from . import artists
 from . import calibration
@@ -13,22 +24,3 @@ from . import fit
 from . import google_drive
 from . import kit
 from . import units
-
-
-### define ####################################################################
-
-
-# WrightTools version format: a.b.c
-# a - major release (potentially breaks old scripts)
-# b - minor release
-# c - bugfix
-__version__ = '2.8.0'
-
-
-### create temp folder if none exists #########################################
-
-
-_temp_dir = _os.path.join(_os.path.dirname(__file__), 'temp')
-if not _os.path.isdir(_temp_dir):
-    _os.mkdir(_temp_dir)
-
