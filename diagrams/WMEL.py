eee43d99abf2607896918d81c6f29f54ee033619
neffmallon <mallonna@gmail.com>
2017-07-12 16:26:15 -0500

    Update to WMEL Subplot class
    Now supports important options for WMEL diagram creation, inlcuding
    lables and arrowhead control

--- diagrams/WMEL.py
+++ diagrams/WMEL.py
@@ -13,7 +13,9 @@ import matplotlib.pyplot as plt
 
 class Subplot:
     
-    def __init__(self, ax, energies, interactions, title='', state_linestyles=None):
+    def __init__(self, ax, energies, number_of_interactions=4, title='',
+                 title_font_size=16,state_names=None, virtual=[None],
+                 state_font_size=8, state_text_buffer=0.5, label_side='left'):
         '''
         Subplot.
         
@@ -23,37 +25,60 @@ class Subplot:
             The axis.
         energies : 1D array-like
             Energies (scaled between 0 and 1)
-        interactions : integer
+        number_of_interactions : integer
             Number of interactions in diagram.
         title : string (optional)
             Title of subplot. Default is empty string.
-        state_linestyles : iterable of matplotlib linestyles
-            Linestyles of each state (horizontal) line. If None, a solid line
-            will be drawn for each state. Deafault is None.
+        state_names: list of str (optional)
+            list of the names of the states
+        virtual: list of ints (optional)
+            list of indexes of any vitual energy states
+        state_font_size: numtype (optional)
+            font size for the state lables
+        state_text_buffer: numtype (optional)
+            space between the energy level bars and the state labels
         '''
         self.ax = ax
-        self.interactions = interactions
-        # plot energies
-        if state_linestyles is None:
-            state_linestyles = ['-'] * interactions
         self.energies = energies
-        for energy, linestyle in zip(self.energies, state_linestyles):
-            self.ax.axhline(energy, color='k', linewidth=3, ls=linestyle, zorder=5)
+        self.interactions = number_of_interactions
+        self.state_names = state_names
+
+        # Plot Energy Levels
+        for i in range(len(self.energies)):
+            if i in virtual: linestyle = '--'
+            else: linestyle = '-'
+            self.ax.axhline(self.energies[i], color='k', linewidth=2,
+                            ls=linestyle, zorder=5)     
+        
+        # add state names
+        if type(state_names)==list:
+            for i in range(len(self.energies)):
+                if label_side == 'left':
+                    ax.text(-state_text_buffer, energies[i], state_names[i],
+                            fontsize = state_font_size, verticalalignment ='center',
+                            horizontalalignment ='center')
+                elif label_side == 'right':
+                    ax.text(1+state_text_buffer, energies[i], state_names[i],
+                            fontsize = state_font_size, verticalalignment ='center',
+                            horizontalalignment ='center')
+        # calculate interaction_positons 
+        self.x_pos = np.linspace(0, 1, number_of_interactions)
         # set limits
         self.ax.set_xlim(-0.1, 1.1)
         self.ax.set_ylim(-0.01, 1.01)
         # remove guff
         self.ax.axis('off')
         # title
-        self.ax.set_title(title, fontsize=16)
+        self.ax.set_title(title, fontsize=title_font_size)
 
-    def add_arrow(self, index, between, kind, label='', font_size=14, color='k'):
+    def add_arrow(self, index, between, kind, label = '', head_length = 0.075,
+                  head_aspect = 2, font_size = 7, color = 'k'):
         '''
         Add an arrow to the WMEL diagram.
         
         Parameters
         ----------
-        index : integer or 2-element iterable of integers
+        index : integer 
             The interaction, or start and stop interaction for the arrow.
         between : 2-element iterable of integers
             The inital and final state of the arrow
@@ -61,36 +86,59 @@ class Subplot:
             The kind of interaction.
         label : string (optional)
             Interaction label. Default is empty string.
+        head_length: number (optional)
+            size of arrow head
         font_size : number (optional)
             Label font size. Default is 14.
         color : matplotlib color (optional)
             Arrow color. Default is black.
+        
+        Returns
+        -------
+        [line,arrow_head,text]
         '''
-        # xi ------------------------------------------------------------------
         if hasattr(index, 'index'):
-            xi = list(index)
+            x_pos = list(index)
         else:
-            xi = [index]*2
-        xi = [np.linspace(0, 1, self.interactions)[i] for i in xi]
-        # yi ------------------------------------------------------------------
-        yi = [self.energies[i] for i in between]
-        length = abs(yi[0] - yi[1])
-        # line ----------------------------------------------------------------
+            x_pos = [index]*2
+        x_pos = [np.linspace(0, 1, self.interactions)[i] for i in x_pos]
+        
+        # calculate arrow length
+        arrow_length = self.energies[between[1]] - self.energies[between[0]]
+        arrow_end = self.energies[between[1]]
+        if arrow_length > 0:
+            direction = 1
+            y_poss = [self.energies[between[0]], self.energies[between[1]] - head_length]
+        elif arrow_length < 0:
+            direction = -1
+            y_poss = [self.energies[between[0]], self.energies[between[1]] + head_length]
+        else:
+            raise ValueError('between invalid!')
+        
+        length = abs(y_poss[0] - y_poss[1])
         if kind == 'ket':
-            line = self.ax.plot(xi, yi, linestyle='-', color=color, linewidth=2, solid_capstyle='round')
+            line = self.ax.plot(x_pos, y_poss, linestyle = '-', color = color,
+                                linewidth = 2,zorder=9)
         elif kind == 'bra':
-            line = self.ax.plot(xi, yi, linestyle='--', color=color, linewidth=2, dash_capstyle='round', dashes=(1, 2))
+            line = self.ax.plot(x_pos, y_poss, linestyle = '--', color = color,
+                                linewidth = 2,zorder=9)
         elif kind == 'out':
-            yio = np.linspace(yi[0], yi[1]+0.05, 100)
-            xio = np.sin((yio - yio[0])*int((1/length)*20)*2*np.pi*length)/40 + xi[1]
-            line = self.ax.plot(xio, yio, linestyle = '-', color = color, linewidth = 2, solid_capstyle='butt')
-        # arrow head ----------------------------------------------------------
-        dx = xi[1] - xi[0]
-        dy = yi[1] - yi[0]
-        xytext = (xi[1]-dx*1e-2, yi[1]-dy*1e-2)
-        annotation = self.ax.annotate('', xy=(xi[1], yi[1]), xytext=xytext, arrowprops=dict(fc=color, ec=color, shrink=0), size=25)
-        # text ----------------------------------------------------------------
-        text = self.ax.text(np.mean(xi), -0.025, label, fontsize=font_size, horizontalalignment='center', va='top')
+            yi = np.linspace(y_poss[0], y_poss[1], 100)
+            xi = np.sin((yi - y_poss[0])*int((1/length)*20)*2*np.pi*length)/40 + x_pos[0]
+            line = self.ax.plot(xi, yi, linestyle = '-', color = color,
+                                linewidth = 2, solid_capstyle='butt',zorder=9)
+        else:
+            raise ValueError('kind is not \'ket\', \'out\', or \'bra\'.')
+        # add arrow head
+        arrow_head = self.ax.arrow(x_pos[1], arrow_end - head_length * direction, 
+                                   0, 0.0001*direction,
+                                   head_width=head_length*head_aspect, 
+                                   head_length=head_length,
+                                   fc=color, ec=color, linestyle='solid',
+                                   linewidth=0, zorder=10)
+        # add text
+        text = self.ax.text(np.mean(x_pos), -0.1, label, fontsize=font_size, horizontalalignment='center')
+        return line, arrow_head, text
 
 
 ### artist ####################################################################
@@ -148,7 +196,7 @@ class Artist:
         plot = self.subplots[diagram[1]][diagram[0]]
         plot.cla()
         
-    def add_arrow(self, diagram, number, between, kind, label = '', head_size = 0.075, font_size = 7, color = 'k'):
+    def add_arrow(self, diagram, number, between, kind, label = '', head_length = 0.075, font_size = 7, color = 'k'):
         '''
         kind one in [ket, bra, out] \n
         returns [line, arrow_head, text]
@@ -160,10 +208,10 @@ class Artist:
         arrow_end = self.energies[between[1]]
         if arrow_length > 0:
             direction = 1
-            y_poss = [self.energies[between[0]], self.energies[between[1]] - head_size]
+            y_poss = [self.energies[between[0]], self.energies[between[1]] - head_length]
         elif arrow_length < 0:
             direction = -1
-            y_poss = [self.energies[between[0]], self.energies[between[1]] + head_size]
+            y_poss = [self.energies[between[0]], self.energies[between[1]] + head_length]
         else:
             print('between invalid!')
             return
@@ -179,10 +227,10 @@ class Artist:
             xi = np.sin((yi - y_poss[0])*int((1/length)*20)*2*np.pi*length)/40 + x_pos
             line = subplot.plot(xi, yi, linestyle = '-', color = color, linewidth = 2, solid_capstyle='butt')
         # add arrow head
-        arrow_head = subplot.arrow(self.x_pos[number], arrow_end - head_size * direction, 
+        arrow_head = subplot.arrow(self.x_pos[number], arrow_end - head_length * direction, 
                                    0, 0.0001*direction,
-                                   head_width=head_size*2, 
-                                   head_length=head_size,
+                                   head_width=head_length*2, 
+                                   head_length=head_length,
                                    fc=color, ec=color, linestyle='solid', linewidth=0)
         # add text
         text = subplot.text(self.x_pos[number], -0.1, label, fontsize=font_size, horizontalalignment='center')
