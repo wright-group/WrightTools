efb5d5320e748ed2b72224ce11acd80dc0b0a789
Blaise Thompson <blaise@untzag.com>
2017-07-16 10:04:11 -0500

    2.13.0 setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,14 @@ setup(
     packages=find_packages(),
     install_requires=['h5py',
                       'matplotlib', 
-				      'numpy',
+		      'numpy',
                       'python-dateutil',
                       'pytz',
                       'scipy'],
-    version='2.12.0',
+    version='2.13.0',
     description='A package for processing multidimensional specroscopy data.',
     author='Blaise Thompson',
     license='MIT',
     author_email='blaise@untzag.com',
     url='https://github.com/wright-group/WrightTools',
 )
-
