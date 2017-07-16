88af49a6cbdbffe7876ac84f034e97897a35f324
Blaise Thompson <blaise@untzag.com>
2017-07-16 15:53:00 -0500

    2.13.2

--- WrightTools/__init__.py
+++ WrightTools/__init__.py
@@ -20,7 +20,7 @@ if not _os.path.isdir(_temp_dir):
 
 # MAJOR.MINOR.PATCH (semantic versioning)
 # major version changes may break backwards compatibility
-__version__ = '2.13.1'
+__version__ = '2.13.2'
 
 # add git branch, if appropriate
 _directory = _os.path.dirname(_os.path.dirname(__file__))
--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 # !/usr/bin/env python
 
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='WrightTools',
     packages=find_packages(),
     install_requires=['h5py', 'matplotlib', 'numpy', 'python-dateutil', 'pytz', 'scipy'],
-    version='2.13.1',
+    version='2.13.2',
     description='A package for processing multidimensional specroscopy data.',
     author='Blaise Thompson',
     license='MIT',
