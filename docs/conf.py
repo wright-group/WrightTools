b24d3db09d546d1cf9fb381eb1886fdd17b5ff74
ksunden <sunden@wisc.edu>
2017-07-19 18:30:46 -0500

    Revert to numpydoc formatting (Reference #81)

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon']
+    'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
