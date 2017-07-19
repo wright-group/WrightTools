954d46d1d7b9a9b13f5736430b12dd015d9e37a4
ksunden <sunden@wisc.edu>
2017-07-19 10:39:34 -0500

    Empty files for #68 #29 #70 #71 #73 #75 #76

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'numpydoc']
+    'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
