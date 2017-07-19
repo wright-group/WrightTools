1cbbca73b5d82d0b7e7ca8ad51cf226efa06d38e
ksunden <sunden@wisc.edu>
2017-07-18 20:06:54 -0500

    Include numpydoc Sphinx extension
    \#51 Developers need this extension (pip install numpydoc)

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode',
+    'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
