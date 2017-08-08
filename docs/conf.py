d8936a8ea813437e8400bd4914a62f11e56f3672
Blaise Thompson <blaise@untzag.com>
2017-08-08 00:00:20 -0500

    more mock

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['numpy', 'h5py']
+MOCK_MODULES = ['matplotlib', 'numpy', 'h5py', 'scipy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
