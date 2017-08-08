bb5e35982295dbaf643ec56b37da7307e00584ec
Blaise Thompson <blaise@untzag.com>
2017-08-07 23:53:22 -0500

    mock

--- docs/conf.py
+++ docs/conf.py
@@ -19,11 +19,24 @@
 #
 import os
 import sys
+from unittest.mock import MagicMock
 import sphinx_gallery
 import math
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../WrightTools'))
+
+
+# -- Mocked Modules -------------------------------------------------------
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['numpy', 'h5py']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
 # -- General configuration ------------------------------------------------
