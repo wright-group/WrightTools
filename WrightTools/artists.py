d4d960f0306a041d61b525a63900f4df9861a0c3
ksunden <sunden@wisc.edu>
2017-08-10 18:03:59 -0500

    Fix import of mpl Axes (remove)

--- WrightTools/artists.py
+++ WrightTools/artists.py
@@ -16,7 +16,6 @@ import numpy as np
 from numpy import r_
 
 import matplotlib
-import matplotlib.axes.Axes
 from matplotlib.axes import SubplotBase, subplot_class_factory
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
