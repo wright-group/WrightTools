b88b3b23b92f9732b3533cada138f5ae3524e18b
Kyle <kyle@kjczech.com>
2017-09-06 16:08:14 -0500

    Issue199 (#214)
    * WrightTools.data.from_spc130 method was created (resolves #199)
    
    WrightTools.data.from_spc130 method was creaetd to load time resolved photoluminescence data objects from native .asc files of the SPC-130 TCSPC module. The SPC-130 TCSPC module is part of the UW-Solar Group Time Resolved PL Imaging Table (TRPLIT)
    
    * WrightTools.data.from_spc130 method was created (resolves #199)
    
    WrightTools.data.from_spc130 method was creaetd to load time resolved photoluminescence data objects from native .asc files of the SPC-130 TCSPC module. The SPC-130 TCSPC module is part of the UW-Solar Group Time Resolved PL Imaging Table (TRPLIT)
    
    * WrightTools.data.from_spc130 method was created (resolves #199)
    
    WrightTools.data.from_spc130 method was creaetd to load time resolved photoluminescence data objects from native .asc files of the SPC-130 TCSPC module. The SPC-130 TCSPC module is part of the UW-Solar Group Time Resolved PL Imaging Table (TRPLIT)
    
    * added wt.data.from_spc130 (resolves #199)
    
    * added wt.data.from_spc130 (resolves #199)
    
    * fixed PEP8 issues associated with wt.data.from_spc130
    
    * fixed PEP8 more issues associated with wt.data.from_spc130
    
    * fixed PEP8 more issues associated with wt.data.from_spc130
    
    * fixed PEP8 more issues associated with wt.data.from_spc130
    
    * style
    
    * fixed docstring issues in from_spc130 method
    
    * fixed error messaging in from_spc130 method
    
    * fixed error messaging in from_spc130 method
    
    * from_spcm
    
    * test spcm

--- WrightTools/data.py
+++ WrightTools/data.py
@@ -10,6 +10,7 @@
 .. _kaiser window: https://en.wikipedia.org/wiki/Kaiser_window
 .. _scipy ndimage:
     http://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.interpolation.zoom.html
+.. _spcm: http://www.becker-hickl.com/software/spcm.htm
 """
 
 
@@ -518,6 +519,7 @@ class Data:
         self.channels = channels
         self.name = name
         self.source = source
+        self.attrs = {}
         # update
         self._update()
         # reserve a copy of own self at this stage
@@ -2866,6 +2868,82 @@ def from_shimadzu(filepath, name=None, verbose=True):
     signal = Channel(data[1], 'sig', file_idx=1, signed=False)
     data = Data([x_axis], [signal], source='Shimadzu', name=name)
     # return --------------------------------------------------------------------------------------
+    return data
+
+
+def from_spcm(filepath, name=None, delimiter=',', format=None, verbose=True):
+    """Create a data object from Becker & Hickl spcm_ software.
+
+    Parameters
+    ----------
+    filepath : string
+        Path to SPC-130 .asc file.
+    name : string (optional)
+        Name to give to the created data object. If None, filename is used.
+        Default is None.
+    delimiter : string (optional)
+        The string used to separate values. Default is ','.
+    format : {'ascii'} (optional)
+        Force file to be interpreted as a specific format. Default is None
+        (autorecognized).
+    verbose : boolean (optional)
+        Toggle talkback. Default is True.
+
+    Returns
+    -------
+    WrightTools.data.Data object
+    """
+    # check filepath
+    if not os.path.isfile(filepath):
+        raise wt_exceptions.FileNotFound(path=filepath)
+    if not filepath.endswith('asc'):
+        w = wt_exceptions.WrongFileTypeWarning.warn(filepath, 'asc')
+        warnings.warn(w)
+    # create headers dictionary
+    headers = collections.OrderedDict()
+    with open(filepath) as f:
+        while True:
+            line = f.readline().strip()
+            if len(line) == 0:
+                break
+            else:
+                key, value = line.split(':', 1)
+                if key.strip() == 'Revision':
+                    headers['resolution'] = int(value.strip(' bits ADC'))
+                else:
+                    headers[key.strip()] = value.strip()
+    # import data
+    # now import file as a local var as comma-delimited .asc file
+    arr = np.genfromtxt(filepath, skip_header=(len(headers) + 2), skip_footer=1,
+                        delimiter=delimiter).T
+    # unexpected delimiter handler
+    if np.any(np.isnan(arr)):
+        # delimiter warning dictionary
+        delim_dict = {',': 'comma',
+                      ' ': 'space',
+                      '\t': 'tab',
+                      ';': 'semicolon',
+                      ':': 'colon'}
+        warnings.warn('file is not %s-delimited! Trying other delimiters.' % delim_dict[delimiter])
+        for delimiter in delim_dict.keys():
+            arr = np.genfromtxt(filepath, skip_header=len(headers) + 2, skip_footer=1,
+                                delimiter=delimiter).T
+            if not np.any(np.isnan(arr)):
+                print('file is %s-delimited.' % delim_dict[delimiter])
+                break
+        else:
+            error = '''Unable to load data file.
+                       Data object not created!
+                       Please check that your file is formatted properly.'''
+            raise RuntimeError(error)
+    # construct data
+    x_axis = Axis(arr[0], 'ns', name='time')
+    signal = Channel(arr[1], 'sig', name='counts', signed=False)
+    data = Data([x_axis], [signal], source='SPC_130', name=name)
+    data.attrs.update(headers)
+    if verbose:
+        print('data object created!')
+    # return
     return data
 
 
