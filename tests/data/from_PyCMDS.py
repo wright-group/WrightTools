e64499373f02d05c83b1e0d961d01a5a7730f5b0
Kyle Sunden <ksunden@users.noreply.github.com>
2017-09-06 12:11:42 -0500

    Make Function name unique (#237)

--- tests/data/from_PyCMDS.py
+++ tests/data/from_PyCMDS.py
@@ -18,7 +18,7 @@ def test_wm_w2_w1_000():
     assert data.axis_names == ['wm', 'w2', 'w1']
 
 
-def test_wm_w2_w1_000():
+def test_wm_w2_w1_001():
     p = datasets.PyCMDS.wm_w2_w1_001
     data = wt.data.from_PyCMDS(p)
     assert data.shape == (29, 11, 11)
