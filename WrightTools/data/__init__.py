f2726022c4c923160f47c4786657e1280f961f95
Blaise Thompson <blaise@untzag.com>
2017-09-06 21:49:20 -0500

    break up data module (resolves #240)

---
+++ WrightTools/data/__init__.py
@@ -0,0 +1,15 @@
+"""Data class and associated."""
+# flake8: noqa
+
+
+from .brunold import *
+from .cary import *
+from .colors import *
+from .jasco import *
+from .kent import *
+from .main import *
+from .ocean_optics import *
+from .pycmds import *
+from .shimadzu import *
+from .spcm import *
+from .tensor27 import *
