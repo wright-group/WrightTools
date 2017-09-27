562461980cc41748b8920f5e2ee1db55a9e54aa5
p770193 <p770193@users.noreply.github.com>
2017-09-27 18:58:16 -0500

    M-factor fix
    data.m now normalizes the correct channel.  resolves #306

--- WrightTools/data/_data.py
+++ WrightTools/data/_data.py
@@ -1239,7 +1239,7 @@ class Data:
                 ai = Ai(axi.points)
                 Mi = mi(ai)
                 # apply Mi to channel
-                self.channels[i].values /= Mi
+                self.channels[channel].values /= Mi
                 # invert back out of the transpose
                 t_inv = [t_order.index(j) for j in range(len(t_order))]
                 if verbose:
