61d9350b67c9aa9cd2ab5271300f5ea09414dcb6
Blaise Thompson <blaise@untzag.com>
2017-10-05 17:07:09 -0700

    update logo

--- logo/logo.py
+++ logo/logo.py
@@ -1,7 +1,7 @@
 """Generate WrightTools logo."""
 
 
-### import ########################################################################################
+# --- import --------------------------------------------------------------------------------------
 
 
 import os
@@ -15,73 +15,85 @@ import matplotlib.pyplot as plt
 import matplotlib.patheffects as PathEffects
 
 import WrightTools as wt
-wt.artists.apply_rcparams('publication')
 
 
-### define ########################################################################################
+# --- define --------------------------------------------------------------------------------------
 
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 cmap = wt.artists.colormaps['default']
+cmap.set_under(color='w', alpha=0)
+
+text_effects = [PathEffects.withStroke(linewidth=5, foreground="w")]
 
 
-### logo ##########################################################################################
+# --- logo ----------------------------------------------------------------------------------------
+
+
+wt.artists.apply_rcparams('publication')
+matplotlib.rcParams['font.monospace'] = "DejaVu Sans Mono"
+matplotlib.rcParams['font.family'] = "monospace"
+matplotlib.rcParams['text.usetex'] = False
 
 
 # get arrays
 p = os.path.join(here, 'peak.h5')
 h5 = h5py.File(p)
-xi = np.array(h5['xi'])
-yi = np.array(h5['yi'])
-zi = np.array(h5['zi'])
+xi = np.array(h5['yi'])
+yi = np.array(h5['xi'])
+zi = np.transpose(np.array(h5['zi']))
 
 # process
 zi = np.log10(zi)
+zi = zi - np.nanmin(zi)
+zi = zi / np.nanmax(zi)
+xi, yi, zi = wt.kit.zoom2D(xi, yi, zi)
 
 # create figure
-fig, gs = wt.artists.create_figure(width=5)
-ax = plt.subplot(gs[0, 0])
+fig = plt.figure()
+ax = plt.subplot(aspect='equal')
 
-# pcolor
-X, Y, Z = wt.artists.pcolor_helper(xi, yi, zi)
-ax.pcolor(X, Y, Z, cmap=cmap)
+cutoff = 0.58
+levels = np.linspace(cutoff, np.nanmax(zi), 10)
 
-# contour
-xi, yi, zi = wt.kit.zoom2D(xi, yi, zi)
-levels = np.linspace(zi.min(), zi.max(), 11)
-ax.contour(xi, yi, zi, levels=levels, colors='k', lw=5, alpha=0.25)
+# contours
+ax.contourf(xi, yi, zi, cmap=cmap, alpha=1, vmin=cutoff, vmax=np.nanmax(zi), levels=levels)
+ax.contour(xi, yi, zi, levels=levels, colors='k', lw=5, alpha=0.5)
+ax.contour(xi, yi, zi, levels=[cutoff], colors='k', lw=5, alpha=1)
 
 # decorate
-ax.set_xlim(xi.min(), xi.max())
-ax.set_ylim(yi.min(), yi.max())
-wt.artists.set_ax_spines(ax=ax, lw=7)
+ax.set_xlim(5500, 8500)
+ax.set_ylim(5500, 8500)
+wt.artists.set_ax_spines(ax=ax, lw=0)
 ax.axes.get_xaxis().set_visible(False)
 ax.axes.get_yaxis().set_visible(False)
-ax.text(7000, 7000, r'$\mathbf{\omega\tau}$', ha='center', va='center',
-        fontsize=165, path_effects=[PathEffects.withStroke(linewidth=5, foreground="w")])
+
+# text
+ax.text(6250, 7500, 'ω', ha='center', va='center', fontsize=200, path_effects=text_effects)
+ax.text(7750, 6500, 'τ', ha='center', va='center', fontsize=200, path_effects=text_effects)
 
 # save
-plt.savefig('logo.png', dpi=300, bbox_inches='tight', pad_inches=0)
-plt.savefig('logo.pdf', bbox_inches='tight', pad_inches=0)
+plt.savefig('logo.png', dpi=300, bbox_inches='tight', pad_inches=0, transparent=True)
 plt.close(fig)
 
 
-### favicon ######################################################################################
+# --- favicon -------------------------------------------------------------------------------------
 
 
 # create figure
-fig, gs = wt.artists.create_figure(width=5)
-ax = plt.subplot(gs[0, 0])
+fig = plt.figure()
+ax = plt.subplot(aspect='equal')
 
 # decorate
-ax.set_xlim(xi.min(), xi.max())
-ax.set_ylim(yi.min(), yi.max())
-wt.artists.set_ax_spines(ax=ax, lw=7, c='none')
+ax.set_xlim(6000, 8000)
+ax.set_ylim(6000, 8000)
+wt.artists.set_ax_spines(ax=ax, lw=0)
 ax.axes.get_xaxis().set_visible(False)
 ax.axes.get_yaxis().set_visible(False)
-ax.text(7000, 7000, r'$\mathbf{\omega\tau}$', ha='center', va='center',
-        fontsize=165, path_effects=[PathEffects.withStroke(linewidth=25, foreground="w")])
+
+# text
+ax.text(7000, 7000, 'ωτ', ha='center', va='center', fontsize=200, path_effects=text_effects)
 
 # save
 plt.savefig('favicon.png', dpi=19, bbox_inches='tight', pad_inches=0, transparent=True)
