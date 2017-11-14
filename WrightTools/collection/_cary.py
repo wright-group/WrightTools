05478568255d34167d07b6b76305b43e3b806c69
ksunden <sunden@wisc.edu>
2017-11-14 00:35:16 -0600

    Make Cary work with HDF5 (closes #349, closes #154)

--- WrightTools/collection/_cary.py
+++ WrightTools/collection/_cary.py
@@ -4,41 +4,49 @@
 # --- import --------------------------------------------------------------------------------------
 
 
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 import os
+import re
 
 import numpy as np
 
-from ._data import Axis, Channel, Data
 from .. import exceptions as wt_exceptions
+from ._collection import Collection
 
 
 # --- define --------------------------------------------------------------------------------------
 
 
-__all__ = ['from_Cary50']
+__all__ = ['from_Cary']
 
 
 # --- from function -------------------------------------------------------------------------------
 
 
-def from_Cary50(filepath, verbose=True):
-    """Create a data object from a Cary 50 UV VIS absorbance file.
+def from_Cary(filepath, parent=None, verbose=True):
+    """Create a data object from a Cary UV VIS absorbance file.
+    
+    We hope to support as many Cary instruments and datasets as possible.
+    This function has been tested with data collected on a Cary50 UV/VIS spectrometer.
+    If any alternate instruments are found not to work as expected, please
+    submit a bug report on our `issue tracker`__.
+
+    __issue tracker: github.com/wright-group/WrightTools/issues
 
     .. plot::
 
         >>> import WrightTools as wt
         >>> from WrightTools import datasets
-        >>> p = datasets.Cary50.CuPCtS_H2O_vis
-        >>> data = wt.data.from_Cary50(p)[0]
-        >>> artist = wt.artists.mpl_1D(data)
+        >>> p = datasets.Cary.CuPCtS_H2O_vis
+        >>> data = wt.data.from_Cary(p)[0]
+        >>> artist = wt.artists.Quick1D(data)
         >>> artist.plot()
 
     Parameters
     ----------
     filepath : string
-        Path to Cary50 output file (.csv).
+        Path to Cary output file (.csv).
+    parent : WrightTools.Collection
+        A collection object in which to place a collection of Data objects.
     verbose : boolean (optional)
         Toggle talkback. Default is True.
 
@@ -48,8 +56,6 @@ def from_Cary50(filepath, verbose=True):
         New data object.
     """
     # check filepath
-    if not os.path.isfile(filepath):
-        raise wt_exceptions.FileNotFound(path=filepath)
     filesuffix = os.path.basename(filepath).split('.')[-1]
     if filesuffix != 'csv':
         wt_exceptions.WrongFileTypeWarning.warn(filepath, 'csv')
@@ -57,6 +63,7 @@ def from_Cary50(filepath, verbose=True):
     lines = []
     with open(filepath, 'r') as f:
         header = f.readline()
+        columns = f.readline()
         while True:
             line = f.readline()
             if line == '\n' or line == '':
@@ -65,19 +72,23 @@ def from_Cary50(filepath, verbose=True):
                 clean = line[:-2]  # lines end with ',/n'
                 lines.append(np.fromstring(clean, sep=','))
     header = header.split(',')
-    lines = [i for i in lines if len(i) > 0]
+    columns = columns.split(',')
+    #lines = [i for i in lines if len(i) > 0]
     arr = np.array(lines).T
     # chew through all scans
-    datas = []
-    indicies = np.arange(len(header) // 2) * 2
-    for i in indicies:
-        axis = Axis(arr[i], 'nm', name='wm')
-        signal = Channel(arr[i + 1], name='absorbance', label='absorbance', signed=False)
-        data = Data([axis], [signal], source='Cary 50', name=header[i])
-        datas.append(data)
+    datas = Collection(name='cary', parent=parent, edit_local=parent is not None)
+    for i in range(0, len(header)-1, 2):
+        print(columns[i], columns[i+1])
+        r = re.compile("[ \t\(\)]+")
+        spl = r.split(columns[i])
+        ax = spl[0].lower() if len(spl) > 0 else None
+        units = spl[1].lower() if len(spl) > 1 else None
+        dat = datas.create_data(header[i], kind='Cary', source=filepath)
+        dat.create_axis(ax, arr[i], units)
+        dat.create_channel(columns[i+1].lower(), arr[i+1])
     # finish
     if verbose:
-        print('{0} data objects successfully created from Cary 50 file:'.format(len(indicies)))
+        print('{0} data objects successfully created from Cary file:'.format(len(datas)))
         for i, data in enumerate(datas):
-            print('  {0}: {1}'.format(i, data.name))
+            print('  {0}: {1}'.format(i, data))
     return datas
