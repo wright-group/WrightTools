a46740206087b5ccdee78347cd0debaf0ed9c7e0
Blaise Thompson <blaise@untzag.com>
2017-12-23 16:17:57 -0600

    JASCO

--- WrightTools/data/_jasco.py
+++ WrightTools/data/_jasco.py
@@ -23,17 +23,17 @@ __all__ = ['from_JASCO']
 # --- from function -------------------------------------------------------------------------------
 
 
-def from_JASCO(filepath, name=None, collection=None, verbose=True):
-    """Create a data object from a JASCO UV-VIS NIR file.
+def from_JASCO(filepath, name=None, parent=None, verbose=True):
+    """Create a data object from JASCO UV-Vis spectrometers.
 
     Parameters
     ----------
-    filepath : string
-        Path to JASCO output file (.txt).
+    filepath : string, list of strings, or array of strings
+        Path to .txt file.
     name : string (optional)
         Name to give to the created data object. If None, filename is used.
         Default is None.
-    collection : WrightTools.Collection (optional)
+    parent : WrightTools.Collection (optional)
         Collection to place new data object within. Default is None.
     verbose : boolean (optional)
         Toggle talkback. Default is True.
@@ -41,32 +41,31 @@ def from_JASCO(filepath, name=None, collection=None, verbose=True):
     Returns
     -------
     data
-        New data object.
+        New data object(s).
     """
     # parse filepath
     if not os.path.isfile(filepath):
         raise wt_exceptions.FileNotFound(path=filepath)
-    filesuffix = os.path.basename(filepath).split('.')[-1]
-    if filesuffix != 'txt':
+    if not filepath.endswith('txt'):
         wt_exceptions.WrongFileTypeWarning.warn(filepath, 'txt')
     # parse name
     if not name:
-        name = filepath
+        name = os.path.basename(filepath).split('.')[0]
     # create data
     kwargs = {'name': name, 'kind': 'JASCO', 'source': filepath}
-    if collection is None:
+    if parent is None:
         data = Data(**kwargs)
     else:
-        data = collection.create_data(**kwargs)
+        data = parent.create_data(**kwargs)
     # array
     arr = np.genfromtxt(filepath, skip_header=18).T
-    # construct data
-    data.create_axis(name='wm', points=arr[0], units='nm')
-    data.create_channel(name='channel', values=arr[1])
+    # chew through all scans
+    data.create_variable(name='energy', values=arr[0], units='nm')
+    data.create_channel(name='signal', values=arr[1])
+    data.transform(['energy'])
     # finish
     if verbose:
         print('data created at {0}'.format(data.fullpath))
-        print('  kind: {0}'.format(data.kind))
-        print('  range: {0} to {1} (nm)'.format(data.wm[0], data.wm[-1]))
+        print('  range: {0} to {1} (nm)'.format(data.energy[0], data.energy[-1]))
         print('  size: {0}'.format(data.size))
     return data
--- tests/data/from_JASCO.py
+++ tests/data/from_JASCO.py
@@ -4,8 +4,6 @@
 # --- import --------------------------------------------------------------------------------------
 
 
-import pytest
-
 import WrightTools as wt
 from WrightTools import datasets
 
@@ -13,28 +11,28 @@ from WrightTools import datasets
 # --- test ----------------------------------------------------------------------------------------
 
 
-@pytest.mark.skip()
 def test_PbSe_batch_1():
     p = datasets.JASCO.PbSe_batch_1
     data = wt.data.from_JASCO(p)
     assert data.shape == (1801,)
-    assert data.axis_names == ['wm']
+    assert data.axis_expressions == ['energy']
+    assert data.units == ('nm',)
     data.close()
 
 
-@pytest.mark.skip()
 def test_PbSe_batch_4_2012_02_21():
     p = datasets.JASCO.PbSe_batch_4_2012_02_21
     data = wt.data.from_JASCO(p)
     assert data.shape == (1251,)
-    assert data.axis_names == ['wm']
+    assert data.axis_expressions == ['energy']
+    assert data.units == ('nm',)
     data.close()
 
 
-@pytest.mark.skip()
 def test_PbSe_batch_4_2012_03_15():
     p = datasets.JASCO.PbSe_batch_4_2012_03_15
     data = wt.data.from_JASCO(p)
     assert data.shape == (1251,)
-    assert data.axis_names == ['wm']
+    assert data.axis_expressions == ['energy']
+    assert data.units == ('nm',)
     data.close()
