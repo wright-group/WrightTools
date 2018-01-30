d0349abc570324a0988244ddea9e35ee85c85a61
Kyle Sunden <sunden@wisc.edu>
2018-01-29 23:04:14 -0600

    INI test boilerplate

---
+++ tests/kit/ini.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+"""Test INI file creation and parsing."""
+
+def test_create():
+    pass
+
+def test_has_option():
+    pass
+
+def test_has_section():
+    pass
+
+def test_dictionary():
+    pass
+
+def test_get_options():
+    pass
+
+def test_sections():
+    pass
+
+def test_read():
+    pass
+
+def test_clear():
+    pass
+
+if __name__ == '__main__':
+    test_create()
+    test_has_option()
+    test_has_section()
+    test_dictionary()
+    test_get_options()
+    test_sections()
+    test_read()
+    #test_clear()
