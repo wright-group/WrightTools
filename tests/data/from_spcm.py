4f7e81ff8aa901b02168b75fee38f644307bef13
Blaise Thompson <blaise@untzag.com>
2018-02-10 17:51:36 -0600

    recover spcm (#470)

--- tests/data/from_spcm.py
+++ tests/data/from_spcm.py
@@ -1,10 +1,8 @@
-"""test from_spcm"""
+"""Test from_spcm."""
 
 
 # --- import --------------------------------------------------------------------------------------
 
-
-import pytest
 
 import WrightTools as wt
 from WrightTools import datasets
@@ -13,10 +11,16 @@ from WrightTools import datasets
 # --- test ----------------------------------------------------------------------------------------
 
 
-@pytest.mark.skip()
 def test_test_data():
     p = datasets.spcm.test_data
     data = wt.data.from_spcm(p)
     assert data.shape == (1024,)
     assert data.axis_names == ('time',)
     data.close()
+
+
+# --- run -----------------------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    test_test_data()
