547371d1fb227b5a88dcc37e8e4165bb5f8283a9
Kyle Sunden <ksunden@users.noreply.github.com>
2018-02-14 19:58:11 -0600

    remove test.ini (#471)
    * remove test.ini
    
    * buffer to random number
    
    * add min

--- tests/dataset/clip.py
+++ tests/dataset/clip.py
@@ -16,7 +16,7 @@ from WrightTools import datasets
 def test_w1_wa():
     p = datasets.PyCMDS.w1_wa_000
     data = wt.data.from_PyCMDS(p)
-    new_max = random.random() * data.array_signal.max()
+    new_max = random.random() * 0.5 * data.array_signal.max() + data.array_signal.min()
     data.array_signal.clip(max=new_max)
     assert data.array_signal.max() <= new_max
     data.close()
