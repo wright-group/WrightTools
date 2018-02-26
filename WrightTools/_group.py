477e846f6eddf21533ac11416ec4399f93584cf0
ksunden <sunden@wisc.edu>
2018-02-25 22:45:59 -0600

    Fix method for adding item_names

--- WrightTools/_group.py
+++ WrightTools/_group.py
@@ -76,9 +76,12 @@ class Group(h5py.Group, metaclass=MetaClass):
         self.__version__
         self.item_names
 
-        if self.parent is not self:
-            self.parent.attrs['item_names'] = np.append(self.parent.attrs['item_names'],
-                                                        self.natural_name.encode())
+        parent = file[parent]
+        if parent.name == self.name:
+            pass  # at root, dont add to item_names
+        elif self.natural_name not in parent.attrs['item_names']:
+            parent.attrs['item_names'] = np.append(parent.attrs['item_names'],
+                                                   self.natural_name.encode())
 
     def __getattr__(self, key):
         """Gets called if attribute not in self.__dict__.
