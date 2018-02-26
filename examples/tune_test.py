8f2fc98925c696e6cf20d329caab877936d44859
Blaise Thompson <blaise@untzag.com>
2018-02-25 19:07:45 -0600

    examples and artists cleanup (#490)
    * tune test
    
    * fringes transform
    
    * quick1D
    
    * quick1D
    
    * rRaman
    
    * rRaman new
    
    * work in progress
    
    * level
    
    * clean-up artists
    
    * local vs global
    
    * overloaded pcolor calls pcolor helper
    
    * review
    
    * whitespace
    
    * pcolor docstring clarification
    
    * review

--- examples/tune_test.py
+++ examples/tune_test.py
@@ -14,21 +14,23 @@ from WrightTools import datasets
 p = datasets.PyCMDS.w1_wa_000
 data = wt.data.from_PyCMDS(p)
 
-fig, gs = wt.artists.create_figure(width='double', cols=[1, 1, 'cbar'])
+fig, gs = wt.artists.create_figure(width='double', cols=[1, 0.25, 1, 'cbar'])
 
 # as taken
 ax = plt.subplot(gs[0, 0])
-ax.pcolor(data.w1__e__wm.full, data.wa.full, data.array_signal)
+ax.pcolor(data)
 wt.artists.set_ax_labels(xlabel=data.w1__e__wm.label, ylabel=data.wa.label)
+ax.grid()
 ax.set_title('as taken', fontsize=20)
 
 # transformed
-ax = plt.subplot(gs[0, 1])
+ax = plt.subplot(gs[0, 2])
 data.transform(['w1', 'wa-w1'])
-ax.pcolor(data.w1.full, data.wa__m__w1.full, data.array_signal)
+ax.pcolor(data)
 wt.artists.set_ax_labels(xlabel=data.w1.label, ylabel=data.wa__m__w1.label)
+ax.grid()
 ax.set_title('transformed', fontsize=20)
 
 # colorbar
 cax = plt.subplot(gs[0, -1])
-wt.artists.plot_colorbar(cax, label='amplitude')
+wt.artists.plot_colorbar(cax, label='intensity')
