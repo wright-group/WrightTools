5c7e734eea84b03fb546465e6826b2cb7e3c0dbb
Blaise Thompson <blaise@untzag.com>
2018-03-16 19:15:45 -0500

    null settr

---
+++ tests/data/channel/null.py
@@ -0,0 +1,28 @@
+"""Tests to do with null."""
+
+
+# --- import --------------------------------------------------------------------------------------
+
+
+import WrightTools as wt
+
+from WrightTools import datasets
+
+
+# --- test ----------------------------------------------------------------------------------------
+
+
+def test_setter():
+    p = datasets.BrunoldrRaman.LDS821_514nm_80mW
+    data = wt.data.from_BrunoldrRaman(p)
+    assert data.signal.null == 0
+    data.signal.null = 5
+    assert data.signal.null == 5
+    data.close()
+
+
+# --- run -----------------------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    test_setter()
