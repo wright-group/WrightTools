1af2f6750f72b9681f00ef8189cdf865a857a327
Blaise Thompson <blaise@untzag.com>
2018-03-20 09:46:19 -0500

    remove empty subscripts in labels (#564)
    * remove empty subscripts in labels
    
    * close bracket

--- WrightTools/data/_axis.py
+++ WrightTools/data/_axis.py
@@ -90,7 +90,10 @@ class Axis(object):
         symbol = wt_units.get_symbol(self.units)
         label = r'$\mathsf{' + self.expression
         for v in self.variables:
-            label = label.replace(v.natural_name, '%s_{%s}' % (symbol, v.label))
+            vl = '%s_{%s}' % (symbol, v.label)
+            vl = vl.replace('_{}', '')  # label can be empty, no empty subscripts
+            label = label.replace(v.natural_name, vl)
+        label += '}'
         if self.units_kind:
             units_dictionary = getattr(wt_units, self.units_kind)
             label += r'\,'
