084776bf50ec730313af9c47d10880480d1f6648
ksunden <sunden@wisc.edu>
2018-04-18 23:11:09 -0500

    use ast and re for string2array heavy lifting

--- WrightTools/kit.py
+++ WrightTools/kit.py
@@ -1362,42 +1362,9 @@ def string2array(string, sep='\t'):
     --------
     array2string
     """
-    # discover size
-    size = string.count('\t') + 1
-    # discover dimensionality
-    dimensionality = 0
-    while string[dimensionality] == '[':
-        dimensionality += 1
-    # discover shape
-    shape = []
-    for i in range(1, dimensionality + 1)[::-1]:
-        to_match = '[' * (i - 1)
-        count_positive = string.count(to_match + ' ')
-        count_negative = string.count(to_match + '-')
-        shape.append(count_positive + count_negative)
-    shape[-1] = size / shape[-2]
-    for i in range(1, dimensionality - 1)[::-1]:
-        shape[i] = shape[i] / shape[i - 1]
-    shape = tuple([int(s) for s in shape])
-    # import list of floats
-    lis = string.split(' ')
-    # annoyingly series of negative values get past previous filters
-    lis = flatten_list([i.split('-') for i in lis])
-    for i, item in enumerate(lis):
-        bad_chars = ['[', ']', '\t', '\n']
-        for bad_char in bad_chars:
-            item = item.replace(bad_char, '')
-        lis[i] = item
-    for i in range(len(lis))[::-1]:
-        try:
-            lis[i] = float(lis[i])
-        except ValueError:
-            lis.pop(i)
-    # create and reshape array
-    arr = np.array(lis)
-    arr.shape = shape
-    # finish
-    return arr
+    string = string.replace(sep, ',')
+    string = re.sub(',,+', ',', string)
+    return np.array(ast.literal_eval(string))
 
 
 def string2identifier(s):
