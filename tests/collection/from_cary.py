c17d8b5b6513b3984216b30ab83ec8f62434cb53
Kyle <sunden@wisc.edu>
2018-05-16 10:54:43 -0500

    test

--- tests/collection/from_cary.py
+++ tests/collection/from_cary.py
@@ -14,6 +14,15 @@ def test_CuPCtS_H2O_vis():
     assert data.shape == (141,)
     col.close()
 
+def test_filters():
+    p = datasets.Cary.filters
+    col = wt.collection.from_Cary(p)
+    assert len(col) == 11
+    for d, sh in zip(col.values(), (121, 196, 301, 301, 301, 301, 301, 301, 301, 401, 101)):
+        assert d.axis_names == ('wavelength',)
+        assert d.channels[0].natural_name in ('abs', '%t')
+        assert d.shape == (sh,)
 
 if __name__ == '__main__':
     test_CuPCtS_H2O_vis()
+    test_filters()
