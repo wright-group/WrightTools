0f467621f5352e834a53a276f25b393c971c3c75
Kyle <sunden@wisc.edu>
2018-06-13 14:06:10 -0500

    remove commented line

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
     tests_require=[
         "pytest",
         "pytest-cov",
-        # "pytest-mp",
         "sphinx==1.6.5",
         "sphinx-gallery==0.1.12",
         "sphinx-rtd-theme",
