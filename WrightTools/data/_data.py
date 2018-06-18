13c12ee91d5e4e6fbbeadf591c177d6f991063f4
Kyle Sunden <ksunden@users.noreply.github.com>
2018-06-18 08:15:11 -0500

    Return tuple rather than list (#634)
    * Return tuple rather than list
    
    * Fix location of tuple cast
    
    * reset lazycache rather than track change

--- WrightTools/data/_data.py
+++ WrightTools/data/_data.py
@@ -170,7 +170,7 @@ class Data(Group):
         except (AssertionError, AttributeError):
             self._variables = [self[n] for n in self.variable_names]
         finally:
-            return self._variables
+            return tuple(self._variables)
 
     @property
     def _leaf(self):
@@ -525,7 +525,7 @@ class Data(Group):
         id = self.require_dataset(name=name, data=values, shape=shape, dtype=dtype).id
         variable = Variable(self, id, units=units, **kwargs)
         # finish
-        self.variables.append(variable)
+        self._variables = None
         self.attrs["variable_names"] = np.append(self.attrs["variable_names"], name.encode())
         return variable
 
