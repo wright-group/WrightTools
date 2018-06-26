47ac074dae17ddc75c6d19c0d53d24566f928e2b
Blaise Thompson <blaise@untzag.com>
2018-06-25 21:21:45 -0500

    review

--- tests/data/channel/normalize.py
+++ tests/data/channel/normalize.py
@@ -4,7 +4,7 @@
 # --- import --------------------------------------------------------------------------------------
 
 
-import random
+import numpy as np
 
 import WrightTools as wt
 from WrightTools import datasets
@@ -22,14 +22,14 @@ def test_LDS821():
     data.close()
 
 
-def test_LDS821_mag_random():
+def test_LDS821_mag_0p5():
     p = datasets.BrunoldrRaman.LDS821_514nm_80mW
     data = wt.data.from_BrunoldrRaman(p)
-    mag = random.random() + 0.5
+    mag = 0.5
     data.signal.normalize(mag)
-    assert data.signal.null == 0.
-    assert data.signal.max() == mag
-    assert data.signal.mag() == mag
+    assert np.isclose(data.signal.null, 0.)
+    assert np.isclose(data.signal.max(), mag)
+    assert np.isclose(data.signal.mag(), mag)
     data.close()
 
 
@@ -37,8 +37,8 @@ def test_wigner():
     p = datasets.COLORS.v2p2_WL_wigner
     data = wt.data.from_COLORS(p)
     data.ai0.normalize()
-    assert data.ai0.null == 0.
-    assert data.ai0.max() == 1.
+    assert np.isclose(data.ai0.null, 0.)
+    assert np.isclose(data.ai0.max(), 1.)
     data.close()
 
 
@@ -48,9 +48,9 @@ def test_negative_wigner():
     data.ai0 *= -1
     data.ai0.signed = True
     data.ai0.normalize()
-    assert data.ai0.null == 0.
-    assert data.ai0.min() == -1.
-    assert data.ai0.mag() == 1.
+    assert np.isclose(data.ai0.null, 0.)
+    assert np.isclose(data.ai0.min(), -1.)
+    assert np.isclose(data.ai0.mag(), 1.)
     data.close()
 
 
@@ -60,9 +60,9 @@ def test_negative_wigner_mag_1p1():
     data.ai0 *= -1
     data.ai0.signed = True
     data.ai0.normalize(1.1)
-    assert data.ai0.null == 0.
-    assert data.ai0.min() == -1.1
-    assert data.ai0.mag() == 1.1
+    assert np.isclose(data.ai0.null, 0.)
+    assert np.isclose(data.ai0.min(), -1.1)
+    assert np.isclose(data.ai0.mag(), 1.1)
     data.close()
 
 
@@ -71,7 +71,7 @@ def test_negative_wigner_mag_1p1():
 
 if __name__ == "__main__":
     test_LDS821()
-    test_LDS821_mag_random()
+    test_LDS821_mag_0p5()
     test_wigner()
     test_negative_wigner()
     test_negative_wigner_mag_1p1()
