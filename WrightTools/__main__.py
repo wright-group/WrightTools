6fd4e5e3a5d0e2846e186afbb684600a5cfe46ed
Tom <parker.tom.f@gmail.com>
2018-06-28 14:07:27 -0700

    Comments on __main__.py.

--- WrightTools/__main__.py
+++ WrightTools/__main__.py
@@ -1,27 +1,29 @@
+# --- import --------------------------------------------------------------------------------------
+
+
 import WrightTools as wt
 import argparse
 
 
+# --- define --------------------------------------------------------------------------------------
+
+
 def wt_tree():
     parser = argparse.ArgumentParser(description="Print a given data tree.")
+
+    # Add arguments
     parser.add_argument("path", type=str)
-    parser.add_argument(
-        "--verbose", "-v", action="store_true", help="If verbose. Default is false"
-    )
-    parser.add_argument(
-        "--depth", "-d", "-l", type=int, default=9, help="depth to print (int)"
-    )
+    parser.add_argument("--verbose", "-v", action="store_true", help="Verbose? False by default")
+    parser.add_argument("--depth", "-d", "-l", type=int, default=9, help="depth to print (int)")
     parser.add_argument("internal_path", nargs="?", default="/")
     args = parser.parse_args()
-    obj = wt.open(args.path, edit_local=true)
-    obj = obj[int_path]  # Set the path to local if changed
+
+    # Create the data/collection object
+    obj = wt.open(args.path, edit_local=True)[args.internal_path]
+
+    # Print the tree
+    # If the object is a data object, it doesn't take depth as a parameter
     if isinstance(obj, wt.Data):
         obj.print_tree(verbose=args.verbose)
     else:
         obj.print_tree(verbose=args.verbose, depth=args.depth)
-
-    # a = 1 if cond else 3
-    # obj.print_tree(verbose=args.verbose) if isinstance(obj, wt.Data) else obj.print_tree(verbose=args.verbose, depth=args.depth)
-
-
-# don't forget to black, and copy formatting from other files
