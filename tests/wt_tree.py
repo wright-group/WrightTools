c6800b251527ec9307d3c544ab326c699e0407e7
Tom <parker.tom.f@gmail.com>
2018-06-28 13:01:24 -0700

    black style fixes

--- tests/wt_tree.py
+++ tests/wt_tree.py
@@ -2,14 +2,21 @@
 
 # --- import --------------------------------------------------------------------------------------
 
+
 import subprocess
+
 
 # --- define --------------------------------------------------------------------------------------
 
+
 from WrightTools import datasets
 
+
 def test_tree():
-    subprocess.run(["wt-tree ", datasets.wt5.v1p0p0_perovskite_TA], encoding='utf-8', check=True)
+    subprocess.run(
+        ["wt-tree ", datasets.wt5.v1p0p0_perovskite_TA], encoding="utf-8", check=True
+    )
+
 
 # --- run -----------------------------------------------------------------------------------------
 
