09642b868fb51dab032470e8a6bc9c9fd5a541d4
Thomas Parker <39585486+parkertomf@users.noreply.github.com>
2018-06-28 17:17:07 -0700

    wt-tree entry points (#631)
    * 451 setup.py changes
    
    * functionality of wt-tree without additional parameters beyond the datafile
    
    * Flags and test for wt-tree
    
    * full implementation of verbose, depth, internal_path. Distinguishes data and collection files.
    
    * black style fixes
    
    * black style fixes
    
    * Comments on __main__.py.
    
    * removed faulty tester
    
    * cleanup

---
+++ WrightTools/__main__.py
@@ -0,0 +1,29 @@
+# --- import --------------------------------------------------------------------------------------
+
+
+import WrightTools as wt
+import argparse
+
+
+# --- define --------------------------------------------------------------------------------------
+
+
+def wt_tree():
+    parser = argparse.ArgumentParser(description="Print a given data tree.")
+
+    # Add arguments
+    parser.add_argument("path", type=str)
+    parser.add_argument("--verbose", "-v", action="store_true", help="Verbose? False by default")
+    parser.add_argument("--depth", "-d", "-L", type=int, default=9, help="depth to print (int)")
+    parser.add_argument("internal_path", nargs="?", default="/")
+    args = parser.parse_args()
+
+    # Create the data/collection object
+    obj = wt.open(args.path, edit_local=True)[args.internal_path]
+
+    # Print the tree
+    # If the object is a data object, it doesn't take depth as a parameter
+    if isinstance(obj, wt.Data):
+        obj.print_tree(verbose=args.verbose)
+    else:
+        obj.print_tree(verbose=args.verbose, depth=args.depth)
