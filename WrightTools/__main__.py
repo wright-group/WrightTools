23b48bfab98a5d623d72f2d7927080d896b8eb3c
Tom <parker.tom.f@gmail.com>
2018-07-12 03:21:25 -0700

    Sig figs

--- WrightTools/__main__.py
+++ WrightTools/__main__.py
@@ -3,10 +3,13 @@
 
 import WrightTools as wt
 import argparse
+from math import log10, floor
 
 
 # --- define --------------------------------------------------------------------------------------
-# Entry points from terminal
+# entry points from terminal
+
+# wt-tree
 
 # Print a wt5 file tree
 def wt_tree():
@@ -29,6 +32,21 @@ def wt_tree():
     else:
         obj.print_tree(verbose=args.verbose, depth=args.depth)
 
+
+# wt-convert
+
+def round_sig(value, sig):
+    result = round(value, sig-int(floor(log10(abs(value))))-1)
+    if result - int(result) == 0:
+        result = int(result)
+    return result
+
+def numDigits(n): 
+    return len(list(filter(lambda m:m.isdigit(), str(n))))
+
+def convert_helper(value, orig, dest):
+    return round_sig(wt.units.converter(float(value), orig, dest), numDigits(value))
+
 def wt_convert():
     parser = argparse.ArgumentParser(description="Converts data units.")
     parser.add_argument('args', nargs='*')
@@ -36,6 +54,8 @@ def wt_convert():
 
     # Perhaps more efficient way to do this is to loop backwards so I start with the units
     # Also use the other printer I used for the first assignment
+    # Also try ternary operator
+    # also functions maybe if once I make everything better there are repeats
     units = ["nm", "wn", "eV", "meV", "Hz", "THz", "GHz"]
 
     unitArgs = []
@@ -44,22 +64,25 @@ def wt_convert():
             unitArgs.append(arg)
 
     valueArgs = [x for x in argsList if x not in unitArgs]
-    print(valueArgs)
 
-    # No destination units provided
+    orig = unitArgs[0]
+
+    # No destination units (so report all)
     if len(unitArgs) == 1:
         for value in valueArgs:
-            for unit in units:
-                if unit != unitArgs[0]: # Don't print original units too
-                    print(wt.units.converter(float(value), unitArgs[0], unit), unit)
+            if len(valueArgs) != 1:
+                print(value, orig, "is")
+            for dest in units:
+                if dest != orig: # Don't print original units too
+                    if len(valueArgs) != 1:
+                        print("  ", end='')
+                    print(convert_helper(value, orig, dest), dest)
 
+    # Destination units
     else:
+        dest = unitArgs[1]
         for value in valueArgs:
-                if len(argsList) - len(unitArgs) == 1:
-                    print(wt.units.converter(float(value), unitArgs[0], unitArgs[1]))
-                else:
-                    print(value, unitArgs[0], "is", 
-                          wt.units.converter(float(value), unitArgs[0], unitArgs[1]),
-                          unitArgs[1])
-
-    #print(wt.units.converter(float(argsList[0]), argsList[1], argsList[2]))
+            if len(valueArgs) == 1:
+                print(convert_helper(value, orig, dest))
+            else:
+                print(value, unitArgs[0], "is", convert_helper(value, orig, dest), dest)
