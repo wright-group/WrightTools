c46653598832d51241eb160c15e29a37ab4df60f
Darien Morrow <darienmorrow@gmail.com>
2018-07-16 17:17:52 -0500

    check all values in kit.fluence test

--- tests/kit/fluence.py
+++ tests/kit/fluence.py
@@ -14,4 +14,7 @@ import WrightTools as wt
 
 def test_0():
     out = wt.kit.fluence(1, 2, .1, 1000, 1, "eV", "cm", "ps_t")
-    assert np.isclose(31.8309, out[0], rtol=1e-3)
+    checks = (31.83098, 99336493460095.2, 0.03183098)
+    assert np.isclose(checks[0], out[0], rtol=1e-3)
+    assert np.isclose(checks[1], out[1], rtol=1e-3)
+    assert np.isclose(checks[2], out[2], rtol=1e-3)
