9a60931129d8d3b62b9bdc3ea3621061f33001b7
Kyle Sunden <sunden@wisc.edu>
2018-09-14 17:01:43 -0500

    Fix python 3.5 by adding cast to str

--- WrightTools/__version__.py
+++ WrightTools/__version__.py
@@ -20,18 +20,18 @@ __all__ = ["__version__", "__branch__"]
 
 
 # read from VERSION file
-with open(here / "VERSION") as f:
+with open(str(here / "VERSION")) as f:
     __version__ = f.read().strip()
 
 
 # add git branch, if appropriate
 p = here.parent / ".git"
 if p.is_file():
-    with open(p) as f:
+    with open(str(p)) as f:
         p = p.parent / f.readline()[8:].strip()  # Strip "gitdir: "
 p = p / "HEAD"
 if p.exists():
-    with open(p) as f:
+    with open(str(p)) as f:
         __branch__ = f.readline().rstrip().split(r"/")[-1]
     if __branch__ != "master":
         __version__ += "+" + __branch__
