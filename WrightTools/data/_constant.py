b7f6c963d6ea34b65d610131b132ec95564cc233
Kyle Sunden <sunden@wisc.edu>
2018-09-17 15:11:48 -0500

    Add format and rounding specification properties

--- WrightTools/data/_constant.py
+++ WrightTools/data/_constant.py
@@ -22,8 +22,8 @@ __all__ = ["Constant"]
 class Constant(Axis):
     """Constant class."""
 
-    def __init__(self, parent, expression, units=None):
-        """Data axis.
+    def __init__(self, parent, expression, units=None, format_spec="0.3g", round_spec=None):
+        """Data constant.
 
         Parameters
         ----------
@@ -33,8 +33,16 @@ class Constant(Axis):
             Constant expression.
         units : string (optional)
             Constant units. Default is None.
+        format_spec : string (optional)
+            Format string specification, as passed to :meth:`format`
+            Default is "0.3g"
+        round_spec : int or None (optional)
+            Decimal digits to round to before formatting, as passed to :meth:`round`.
+            Default is None (no rounding).
         """
         super().__init__(parent, expression, units)
+        self.format_spec = format_spec
+        self.round_spec = round_spec
 
     def __repr__(self) -> str:
         return "<WrightTools.Constant {0} = {1} {2} at {3}>".format(
@@ -59,7 +67,7 @@ class Constant(Axis):
                 vl = "%s_{%s}" % (symbol, v.label)
                 vl = vl.replace("_{}", "")  # label can be empty, no empty subscripts
                 label = label.replace(v.natural_name, vl)
-        label += r"\,=\,{}".format(self.value)
+        label += r"\,=\,{}".format(format(round(self.value, self.round_spec), self.format_spec))
         if self.units_kind:
             units_dictionary = getattr(wt_units, self.units_kind)
             label += r"\,"
