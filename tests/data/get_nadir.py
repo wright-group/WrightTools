28647410d0767596df0451eb9023e60e793309bd
Kyle Sunden <sunden@wisc.edu>
2018-11-06 11:17:47 -0600

    TST: Update number in get_nadir

--- tests/data/get_nadir.py
+++ tests/data/get_nadir.py
@@ -10,7 +10,7 @@ def test_nadir():
     p = wt.datasets.PyCMDS.wm_w2_w1_000
     data = wt.data.from_PyCMDS(p)
     data.wm.convert("nm")
-    assert np.allclose(list(data.get_nadir()), [483.0833, 1590., 6250.])
+    assert np.allclose(list(data.get_nadir()), [483.0833, 1590., 1600.])
 
 
 if __name__ == "__main__":
