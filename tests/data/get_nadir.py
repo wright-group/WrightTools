BUG: fix expected units behavior in from_PyCMDS (#768)
* BUG: fix expected units behavior in from_PyCMDS

* Add units import

* TST: Update tests for change in fro_PyCMDS

* TST: Correct units in convert_data

* TST: Update number in get_nadir

tests/data/get_nadir.py
@@ -9,7 +9,8 @@ from WrightTools import datasets
 def test_nadir():
     p = wt.datasets.PyCMDS.wm_w2_w1_000
     data = wt.data.from_PyCMDS(p)
-    assert np.allclose(list(data.get_nadir()), [483.0833, 1590., 6250.])
+    data.wm.convert("nm")
+    assert np.allclose(list(data.get_nadir()), [483.0833, 1590., 1600.])
 
 
 if __name__ == "__main__":