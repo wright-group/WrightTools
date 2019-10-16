1d06075e87d964ecfa564b33cf3fdc32079494b2
Blaise Thompson <blaise@untzag.com>
2019-10-16 10:19:44 -0500

    always include git in version (#927)

--- WrightTools/__version__.py
+++ WrightTools/__version__.py
@@ -33,7 +33,6 @@ p = p / "HEAD"
 if p.exists():
     with open(str(p)) as f:
         __branch__ = f.readline().rstrip().split(r"/")[-1]
-    if __branch__ != "master":
-        __version__ += "+" + __branch__
+    __version__ += "+" + __branch__
 else:
     __branch__ = None
