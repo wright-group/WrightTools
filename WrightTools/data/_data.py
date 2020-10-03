4fa8ddcf9fea7e844d972866573f59a96afb8af5
Kyle Sunden <git@ksunden.space>
2020-10-02 19:13:43 -0500

    Remove print

--- WrightTools/data/_data.py
+++ WrightTools/data/_data.py
@@ -70,7 +70,6 @@ class Data(Group):
             const = Constant(self, expression, units)
             self._constants.append(const)
         self._current_axis_identities_in_natural_namespace = []
-        print(f"File mode: {self.file.mode}")
         if self.file.mode is not None and self.file.mode != "r":
             self._on_constants_updated()
             self._on_axes_updated()
