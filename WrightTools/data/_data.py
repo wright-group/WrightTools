2cbb2ee3f16b9e88ac74630e70cfab57c9326f31
Darien Morrow <darienmorrow@gmail.com>
2021-03-17 15:45:05 -0500

    Convert verbose (#996)
    * (closes #995)
    
    * Update CHANGELOG.md
    
    * silence verbose in convert
    
    * Update CHANGELOG.md

--- WrightTools/data/_data.py
+++ WrightTools/data/_data.py
@@ -1672,7 +1672,7 @@ class Data(Group):
 
         self.transform(expression)
         if units:
-            self.convert(units)
+            self.convert(units, verbose=False)
 
         try:
             positions = [-np.inf] + sorted(list(positions)) + [np.inf]
