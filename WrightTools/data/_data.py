437b6842ce558a77c097a57126383175cc80ce25
Darien Morrow <darienmorrow@gmail.com>
2021-03-17 15:15:01 -0500

    silence verbose in convert

--- WrightTools/data/_data.py
+++ WrightTools/data/_data.py
@@ -1672,7 +1672,7 @@ class Data(Group):
 
         self.transform(expression)
         if units:
-            self.convert(units, verbose=verbose)
+            self.convert(units, verbose=False)
 
         try:
             positions = [-np.inf] + sorted(list(positions)) + [np.inf]
