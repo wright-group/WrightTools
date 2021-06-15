8897a2c7e2317586d9e9f47106205f1cd10723a1
Morrow <dmorrow@anl.gov>
2021-06-15 17:24:20 -0500

    add test for units, or rather add unit, unit tests ;P

--- tests/units.py
+++ tests/units.py
@@ -26,3 +26,29 @@ def test_axis_convert_exception():
 def test_in_mm_conversion():
     assert np.isclose(wt.units.convert(25.4, "mm", "in"), 1.0)
     assert np.isclose(wt.units.convert(1.0, "in", "mm"), 25.4)
+
+
+def unit_registry_test1():
+    values = np.linspace(-1,1,51)
+    arr = np.linspace(0,1,51)
+    d = wt.Data(name='test')
+    try:
+        d.create_variable('Bgood', values=values, units='tesla')
+        d.transform('Bgood')
+    except ValueError :
+        assert False
+    else:
+        assert True
+
+
+def unit_registry_test2():
+    values = np.linspace(-1,1,51)
+    arr = np.linspace(0,1,51)
+    d = wt.Data(name='test')
+    try:
+        d.create_variable('Bbad', values=values, units='Tesla')
+        d.transform('Bbad')
+    except ValueError :
+        assert True
+    else:
+        assert False
