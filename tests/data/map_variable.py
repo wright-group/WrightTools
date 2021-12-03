0efd3fb7908a9ddd0da9113c43241f419978ae4d
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2021-12-03 16:47:51 -0600

    tests

--- tests/data/map_variable.py
+++ tests/data/map_variable.py
@@ -34,9 +34,27 @@ def test_int():
     data.close()
 
 
+def test_excess_data_kwarg_1D():
+    p = datasets.wt5.v1p0p0_perovskite_TA
+    data = wt.open(p).chop("w2", at={"w1=wm": [1.6, "eV"], "d2":[0, "fs"]})[0]
+    mapped = data.map_variable("w2", 11)
+    assert mapped.w2.size == 11
+    data.close()
+
+
+def test_excess_data_kwarg_3D():
+    p = datasets.wt5.v1p0p0_perovskite_TA
+    data = wt.open(p)
+    mapped = data.map_variable("w2", 11)
+    assert mapped.w2.size == 11
+    data.close()
+
+
 # --- run -----------------------------------------------------------------------------------------
 
 
 if __name__ == "__main__":
     test_array()
     test_int()
+    test_excess_data_kwarg_1D()
+    test_excess_data_kwarg_3D()
