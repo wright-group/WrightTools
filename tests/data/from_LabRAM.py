8112eb4cc534177042617b7f74a8ed3db74c3c44
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2022-03-09 22:39:32 -0600

    more tests

--- tests/data/from_LabRAM.py
+++ tests/data/from_LabRAM.py
@@ -1,12 +1,34 @@
 import WrightTools as wt
 from WrightTools.datasets import LabRAM
 
-
-def test_import():
-    d = wt.data.from_LabRAM(LabRAM.raman_linescan)
-    d.print_tree()
+def test_spectral_units():
+    d = wt.data.from_LabRAM(LabRAM.spectrum_nm)
+    assert d.wm.units == "nm"
     d.close()
+    d = wt.data.from_LabRAM(LabRAM.spectrum_wn)
+    assert d.wm.units == "wn"
+    d.close()
+
+def test_import_1D():
+    d = wt.data.from_LabRAM(LabRAM.spectrum_nm)
+    d.close()
+
+def test_import_2D():
+    d = wt.data.from_LabRAM(LabRAM.raman_linescan)
+    d.close()
+
+def test_import_2D_survey():
+    d = wt.data.from_LabRAM(LabRAM.survey_nm)
+    d.close()
+
+def test_import_3D():
+    d = wt.data.from_LabRAM(LabRAM.map_nm)   
+    d.close() 
 
 
 if __name__ == "__main__":
-    test_import()
+    test_spectral_units()
+    test_import_1D()
+    test_import_2D()
+    test_import_2D_survey()
+    test_import_3D()
