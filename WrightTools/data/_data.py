95fa4858d03e4aed71fd43f0fd7085a1aaae1edf
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2022-08-03 21:45:11 -0500

    Update _data.py

--- WrightTools/data/_data.py
+++ WrightTools/data/_data.py
@@ -482,7 +482,7 @@ class Data(Group):
         for k in at.keys():
             if k not in self.axis_names:
                 raise ValueError(f"Axis identifier {k} not in Axes: {self.axis_names}")
-        idx = np.array([slice(None)] * len(self._axes))
+        idx = np.array([slice(None)] * len(self.shape))
         for axis, point in at.items():
             point, units = point
             destination_units = self._axes[self.axis_names.index(axis)].units
