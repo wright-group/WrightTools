60465768eb0a63dfe882246753e480564b6a874e
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2023-06-20 14:27:54 -0500

    Update translate_to_txt.py
    ensure tempfile cleanup
    minimal check for correct output

--- tests/data/translate_to_txt.py
+++ tests/data/translate_to_txt.py
@@ -15,9 +15,23 @@ from tempfile import NamedTemporaryFile
 
 
 def test_datasets_mos2():
-    d = wt.open(datasets.wt5.v1p0p1_MoS2_TrEE_movie).at(w2=[18000, "wn"])
-    tmp = NamedTemporaryFile(delete=False)
-    d.translate_to_txt(tmp.name, verbose=True)
+    d = wt.open(datasets.wt5.v1p0p1_MoS2_TrEE_movie).at(w2=[18000, "wn"])[:5]
+
+    with NamedTemporaryFile(delete=False) as tmp:
+        d.translate_to_txt(tmp.name, verbose=True)
+ 
+        with open(tmp.name, "r") as f:
+            for i in range(100):
+                f.readline()
+            datum_txt = f.readline().split("|")
+            id1, id2 = [_ for _ in map(int, datum_txt[0].split())]
+            values = [_ for _ in map(float, datum_txt[1].split())]
+            datum_wt5 = d[int(id1), int(id2)]
+            for i, vari in enumerate(d.variable_names):
+                # print(vari, values[i], datum_wt5[vari][:])
+                assert (values[i] - datum_wt5[vari][:])**2 <= (1e-4 * values[i])**2
+
+
     d.close()
 
 
