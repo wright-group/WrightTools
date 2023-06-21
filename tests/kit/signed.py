34beb5689c593d307625d4785a8e06bdc51fe83e
rpm4 <rpmcdonnell@wisc.edu>
2023-06-21 14:47:51 -0500

    Create signed.py

---
+++ tests/kit/signed.py
@@ -0,0 +1,26 @@
+"""Test signed data."""
+
+
+# --- import -------------------------------------------------------------------------------------
+
+
+import numpy as np
+
+import WrightTools as wt
+
+
+# --- test ---------------------------------------------------------------------------------------
+
+
+def test_5():
+    arr = np.array([-1, 0, 1])
+    assert wt.kit.signed(arr) == True
+
+
+
+def test_5_multiple():
+    arr = np.array([1, 3, 4, 11, 12])
+    assert wt.kit.signed(arr) == False
+
+
+
