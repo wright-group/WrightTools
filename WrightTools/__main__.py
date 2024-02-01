200c557c28bf37ed8858bffcf6388006925da604
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2024-02-01 11:57:46 -0600

    removing pause from crawl

--- WrightTools/__main__.py
+++ WrightTools/__main__.py
@@ -4,6 +4,7 @@
 import click
 import WrightTools as wt
 
+
 # --- define --------------------------------------------------------------------------------------
 
 
@@ -24,7 +25,6 @@ def tree(path, internal_path, depth=9, verbose=False):
     # open the object
     obj = wt.open(path)[internal_path]
 
-    # If the object is a data object, it doesn't take depth as a parameter
     if isinstance(obj, wt.Data):
         obj.print_tree(verbose=verbose)
     else:
@@ -41,21 +41,21 @@ def load(path):
 
 
 @cli.command(name="crawl", help="Crawl a directory and survey the wt5 objects found.")
-@click.argument("directory", nargs=-1, default=None)
-@click.option("--recursive", "-r", is_flag=True, help="explore all levels of the directory")
+@click.option("--directory", "-d", default=None, help="Directory to crawl.  Defaults to current directory.")
+@click.option("--recursive", "-r", is_flag=True, help="Explore all levels of the directory")
 @click.option(
     "--pausing",
     "-p",
     is_flag=True,
     help="pause at each file readout. Interaction with data is possible.",
 )
-def crawl(directory=(), recursive=False, pausing=False):
+@click.option("--format", "-f", default=None, help="Formatting keys (default only atm)")
+# TODO: write output as an option
+def crawl(directory=None, recursive=False, pausing=False, format=None):
     import glob, os
 
-    if not directory:
+    if directory is None:
         directory = os.getcwd()
-    else:
-        directory = directory[0]
 
     if pausing:
         import code
@@ -65,42 +65,91 @@ def crawl(directory=(), recursive=False, pausing=False):
 
         shell = code.InteractiveConsole(locals={"exit": raise_sys_exit, "quit": raise_sys_exit})
 
-    print("-" * 100)
-    for pathname in glob.iglob("**/*.wt5", root_dir=directory, recursive=recursive):
-        print(pathname)
-        d = wt.open(os.path.join(directory, pathname))
-        if isinstance(d, wt.Data):
-            d.print_tree(verbose=False)
-        elif isinstance(d, wt.Collection):
-            d.print_tree(depth=1, verbose=False)
+    paths = glob.glob("**/*.wt5", root_dir=directory, recursive=recursive)
+    print(f"{len(paths)} wt5 file{'s' if len(paths) != 1 else None} found in {directory}")
+
+    from rich.console import Console
+    from rich.table import Table
+
+    console = Console()
+
+    table = Table(title=directory)
+    table.add_column("", justify="right")  # index
+    table.add_column("path", max_width=60)
+    table.add_column("size (MB)", justify="center")
+    table.add_column("created", max_width=30)
+    table.add_column("name")
+    table.add_column("shape")
+    table.add_column("axes")
+    table.add_column("variables")
+    table.add_column("channels")
+
+
+    for i, pathname in enumerate(paths):
+        path = os.path.join(directory, pathname)
+        d = wt.open(path)
+        infos = [str(i), pathname, str(int(os.path.getsize(path)/1e6)), *_format_entry(d)]
+        table.add_row(*infos)
 
         if pausing:
-            msg = shell.raw_input("Continue [y]/n/interact? ")
-            if msg == "n":
+            msg = shell.raw_input("Interact ([n=no]/y=yes/q=quit)? ")
+            if msg.lower() in ["q", "quit"]:
                 break
-            elif msg == "interact":
-                lines = ["import WrightTools as wt"]
-                if isinstance(d, wt.Data):
-                    lines += [
-                        "import matplotlib.pyplot as plt",
-                        f"path = r'{os.path.join(directory, pathname)}'",
-                        "d = wt.open(path)",
-                    ]
-                elif isinstance(d, wt.Collection):
-                    lines.append(f"c = wt.open(r'{os.path.join(directory, pathname)}')")
-
-                [shell.push(line) for line in lines]
-                banner = "--- INTERACTING --- (to continue, call exit() or quit())\n"
-                banner += "\n".join([">>> " + line for line in lines])
-
-                try:
-                    shell.interact(banner=banner)
-                except SystemExit:
-                    pass
+            elif msg.lower() in ["y", "yes"]:
+                _interact(shell, os.path.join(directory, pathname), isinstance(d, wt.Collection))
+                print("-" * 100)
             else:
                 continue
-        print("-" * 100)
         d.close()
+
+
+from typing import Tuple, Optional
+class TableEntry:
+    created: str = ""
+    name: str = ""
+    shape: Optional(Tuple(int))
+    axes: Optional(Tuple(str))
+    nvars: Optional(int)
+    nchan: Optional(int)
+    def __init__(self, wt5):
+        self.name = wt5.natural_name
+        self.created = wt5.attrs["created"].date
+        if isinstance(wt5, wt.Data):
+            self.shape = wt5.shape
+            self.axes = wt5.axis_expressions
+            self.nvars = len(wt5.variables)
+            self.nchan = len(wt5.channels)
+
+    
+
+
+
+
+def _format_entry(wt5, format=None):
+    if isinstance(wt5, wt.Data):
+        values = [f"{wt5.attrs['created']}", wt5.natural_name, f"{wt5.shape}", f"{wt5.axis_expressions}", str(len(wt5.variables)), str(len(wt5.channels))]
+    elif isinstance(wt5, wt.Collection):
+        values = ["---"] + [wt5.natural_name] + ["---"] * 4
+    else:
+        values = ["---"] + ["<unknown>"] + ["---"] * 4
+    return values
+
+
+def _interact(shell, path, is_collection):
+    lines = [
+        "import WrightTools as wt",
+        "import matplotlib.pyplot as plt",
+        f"{'c' if is_collection else 'd'} = wt.open(r'{path}')",
+    ]
+
+    [shell.push(line) for line in lines]
+    banner = "--- INTERACTING --- (to continue, call exit() or quit())\n"
+    banner += "\n".join([">>> " + line for line in lines])
+
+    try:
+        shell.interact(banner=banner)
+    except SystemExit:
+        pass
 
 
 @cli.command(name="convert")
