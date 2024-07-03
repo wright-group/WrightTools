c34a86131725b8a505663a43e9ad7d937ae7e387
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2024-07-03 08:32:04 -0500

    trapezoid

--- tests/kit/lineshapes.py
+++ tests/kit/lineshapes.py
@@ -6,7 +6,8 @@
 import numpy as np
 
 import WrightTools as wt
-
+# numpy 2.0 compatibility
+trapezoid = np.trapezoid if int(np.__version__.split(".")[0]) > 1 else np.trapz
 
 # --- test ----------------------------------------------------------------------------------------
 
@@ -16,7 +17,7 @@ def test_gaussian():
     x0 = 0
     FWHM = 1
     y = wt.kit.gaussian(x, x0, FWHM, norm="area")
-    assert np.isclose(np.trapz(y, x), 1, rtol=1e-3, atol=1e-3)
+    assert np.isclose(trapezoid(y, x), 1, rtol=1e-3, atol=1e-3)
     y = wt.kit.gaussian(x, x0, FWHM, norm="height")
     assert np.isclose(y.max(), 1, rtol=1e-3, atol=1e-3)
 
@@ -26,7 +27,7 @@ def test_lorentzian_complex():
     x0 = 0
     G = 0.1
     y = wt.kit.lorentzian_complex(x, x0, G, norm="area_int")
-    assert np.isclose(np.trapz(np.abs(y) ** 2, x), 1, rtol=1e-3, atol=1e-3)
+    assert np.isclose(trapezoid(np.abs(y) ** 2, x), 1, rtol=1e-3, atol=1e-3)
     y = wt.kit.lorentzian_complex(x, x0, G, norm="height_imag")
     assert np.isclose(y.imag.max(), 1, rtol=1e-3, atol=1e-3)
 
@@ -36,7 +37,7 @@ def test_lorentzian_real():
     x0 = 0
     G = 0.1
     y = wt.kit.lorentzian_real(x, x0, G, norm="area")
-    assert np.isclose(np.trapz(y, x), 1, rtol=1e-3, atol=1e-3)
+    assert np.isclose(trapezoid(y, x), 1, rtol=1e-3, atol=1e-3)
     y = wt.kit.lorentzian_real(x, x0, G, norm="height")
     assert np.isclose(y.max(), 1, rtol=1e-3, atol=1e-3)
 
