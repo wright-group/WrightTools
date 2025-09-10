c1556f3a0057fab2fae19a6416298fc47c532556
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2025-09-10 18:04:05 -0500

    less docs build testing

--- tests/docs/_sphinx.py
+++ tests/docs/_sphinx.py
@@ -1,10 +1,13 @@
 """Test building the documentation."""
 
+import pytest
+import sys
 from sphinx.cmd import build
 import os
 import shutil
 
 
+@pytest.mark.skipif(not sys.version.startswith("3.11"))
 def test_build_docs():
     docsdir = os.path.abspath(os.path.dirname(__file__)) + "/../../docs"
     exitCode = build.build_main([docsdir, docsdir + "/__testbuild"])
