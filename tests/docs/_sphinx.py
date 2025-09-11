75a773b713dd6e95e73bf3ccad54d1eaee4afc4b
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2025-09-10 21:05:52 -0500

    Update _sphinx.py

--- tests/docs/_sphinx.py
+++ tests/docs/_sphinx.py
@@ -3,21 +3,18 @@
 import pytest
 import sys
 from sphinx.cmd import build
-import os
+import pathlib
 import shutil
 
 
-@pytest.mark.skipif(
-    sys.version_info > (3, 12) or sys.platform == "win32",
-    reason="Only need to build with ubuntu, python 3.11",
-)
+@pytest.mark.skipif(sys.version_info[:2] != (3, 11) or sys.platform == "win32", reason="Only need to build with ubuntu, python 3.11")
 def test_build_docs():
-    docsdir = os.path.abspath(os.path.dirname(__file__)) + "/../../docs"
-    exitCode = build.build_main([docsdir, docsdir + "/__testbuild"])
+    docsdir = pathlib.Path(__file__).resolve().parent.parent.parent / "docs"
+    exitCode = build.build_main([docsdir, docsdir / "__testbuild"])
     # The following code works in sphinx >= 1.7.0
     # exitCode = sphinx.build([docsdir, docsdir + '/__testbuild'])
     assert exitCode == 0
-    shutil.rmtree(docsdir + "/__testbuild")
+    shutil.rmtree(docsdir / "__testbuild")
 
 
 if __name__ == "__main__":
