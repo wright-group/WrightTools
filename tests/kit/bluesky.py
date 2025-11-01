47fab443418384a0e32b262ce5b4848dc55e7538
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2025-11-01 14:06:40 -0500

    bluesky test

---
+++ tests/kit/bluesky.py
@@ -0,0 +1,22 @@
+import WrightTools as wt
+
+
+def test_folderinfo():
+    name1 = "2025-10-27 52433 count 2 beam PL spot2 post d7f183b5"
+    name2 = "2025-10-27 54622 grid_scan_wp spot 3 spectral 6a45457c"
+
+    fi1 = wt.kit.bluesky.parse_folder_name(name1)
+    fi2 = wt.kit.bluesky.parse_folder_name(name2)
+
+    for name, fi in [[name1, fi1], [name2, fi2]]:
+        assert fi is not None
+        assert fi.folder == name
+
+    assert fi1.name == "2 beam PL spot2 post"
+    assert fi2.name == "spot 3 spectral"
+    assert fi1.plan == "count"
+    assert fi2.plan == "grid_scan_wp"
+
+
+if __name__ == "__main__":
+    test_folderinfo()
