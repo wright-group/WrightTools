91be7f90e59aeebbeca6e7494944b40f806ea0fc
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2026-03-13 19:43:15 -0500

    Create test_animate.py

---
+++ tests/artists/test_animate.py
@@ -0,0 +1,23 @@
+import WrightTools as wt
+from WrightTools import datasets
+from matplotlib import pyplot as plt
+
+
+def test_animate2D():
+    """
+    smokescren test: just ensure the ani object initializes
+
+    if you are running this locally, you can view the animation
+    by running this script
+    """
+    d = wt.open(datasets.wt5.v1p0p1_MoS2_TrEE_movie)
+    d.channels[0].signed = True
+    from matplotlib.colors import CenteredNorm
+    from functools import partial
+    ani = wt.artists.animate2D(d, interval=1, norm=partial(CenteredNorm, vcenter=0))
+    return ani
+
+
+if __name__ == "__main__":
+    ani = test_animate2D()
+    plt.show()
