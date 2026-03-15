58bae5956f70f33761be5082859064f0e3a55682
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2026-03-15 12:39:05 -0500

    Update test_animate.py

--- tests/artists/test_animate.py
+++ tests/artists/test_animate.py
@@ -34,7 +34,9 @@ def test_animate_quick2D():
     d = wt.open(datasets.wt5.v1p0p1_MoS2_TrEE_movie)
     d.channels[0].signed = True
 
-    ani = wt.artists.animate_quick2D(d, fa_kwargs=dict(interval=100))
+    quick2D = wt.artists._quick_v2.quick2D(d)
+
+    ani = wt.artists.animate_quick2D(quick2D, fa_kwargs=dict(interval=100))
     return ani
 
 
