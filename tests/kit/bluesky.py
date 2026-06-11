106321791a78a5419670405c5ef2148646f7c429
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2026-06-11 15:13:52 -0500

    test bluesky
    revise folderinfo test

--- tests/kit/bluesky.py
+++ tests/kit/bluesky.py
@@ -2,20 +2,22 @@ import WrightTools as wt
 
 
 def test_folderinfo():
-    name1 = "2025-10-27 52433 count 2 beam PL spot2 post d7f183b5"
-    name2 = "2025-10-27 54622 grid_scan_wp spot 3 spectral 6a45457c"
+    names = [
+        "2025-10-27 52433 count 2 beam PL spot2 post d7f183b5",
+        "2025-10-27 00622 grid_scan_wp spot 3 spectral 6a45457c",
+        "2025-10-27 00010 custom_plan +-5 --testing-- p3uyqhr7",
+    ]
 
-    fi1 = wt.kit.bluesky.parse_folder_name(name1)
-    fi2 = wt.kit.bluesky.parse_folder_name(name2)
+    fis = [wt.kit.bluesky.parse_folder_name(ni) for ni in names]
 
-    for name, fi in [[name1, fi1], [name2, fi2]]:
+    for name, fi in zip(names, fis):
         assert fi is not None
-        assert fi.folder == name
+        assert fi.strf == name
 
-    assert fi1.name == "2 beam PL spot2 post"
-    assert fi2.name == "spot 3 spectral"
-    assert fi1.plan == "count"
-    assert fi2.plan == "grid_scan_wp"
+    assert fis[0].name == "2 beam PL spot2 post"
+    assert fis[1].name == "spot 3 spectral"
+    assert fis[0].plan == "count"
+    assert fis[1].plan == "grid_scan_wp"
 
 
 def test_filter():
